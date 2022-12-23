4362aea29b7c6017585ab829c680cf83c542193d
Yash1234964 <ryash8500@gmail.com>
2022-12-23 20:00:48 +0530

    first question

--- StringAssignment/src/stringAssignment/question1.java
+++ StringAssignment/src/stringAssignment/question1.java
@@ -1,10 +1,31 @@
 package stringAssignment;
-
+import java.util.*;
 public class question1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        System.out.println("Enter the string");
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        
+        s=s.toLowerCase();
+        int l=s.length();
+        int arr[] = new int[26];
+        
+        for(int i=0;i<l;i++) {
+        	if(s.charAt(i)!=' ' && arr[s.charAt(i)-'a']==0)
+        	arr[(s.charAt(i)-'a')]++;
+        }
+        for(int i=0;i<l;i++) {
+        	if(s.charAt(i)!=' ' && arr[s.charAt(i)-'a']>0 )
+        	{
+        		arr[s.charAt(i)-'a']--;
+        		System.out.print(s.charAt(i));
+        	}
+        	if(s.charAt(i)==' ')
+        		System.out.print(" ");
+        }
+        
 	}
 
 }
