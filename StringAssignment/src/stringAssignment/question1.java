eca7fe1e18c759dfeeb51f2bb8dd1a550218fe70
Yash1234964 <ryash8500@gmail.com>
2022-12-25 12:56:26 +0530

    Final Commit

--- StringAssignment/src/stringAssignment/question1.java
+++ StringAssignment/src/stringAssignment/question1.java
@@ -4,7 +4,7 @@ public class question1 {
 
 	public static void main(String[] args) {
 	
-        System.out.println("Enter the string");
+        System.out.println("Enter your the string");
         Scanner sc=new Scanner(System.in);
         String s=sc.nextLine();
         
