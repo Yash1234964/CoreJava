2eb0884b6744b90b8ff84bfe7adeffde6c78764e
Yash1234964 <ryash8500@gmail.com>
2023-01-06 22:15:27 +0530

    Exception commit

--- CoreJava/src/ExceptionPractice/ExceptionDemo.java
+++ CoreJava/src/ExceptionPractice/ExceptionDemo.java
@@ -1,10 +1,29 @@
 package ExceptionPractice;
 
+import java.util.Scanner;
+
 public class ExceptionDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-  System.out.println("ujkgdkas");
+            try {
+            	Scanner sc=new Scanner(System.in);
+            	System.out.println("Enter First Number");
+            	int n1=sc.nextInt();
+            	System.out.println("Enter Second Number");
+            	int n2=sc.nextInt();
+                int n3=n1/n2;
+                System.out.println(n3);
+            			
+            }
+            catch(ArithmeticException a) {
+            	System.out.println("Exception occur");
+            	a.getMessage();
+            	a.getStackTrace();
+            }
+            
+		
+		
 	}
 
 }
