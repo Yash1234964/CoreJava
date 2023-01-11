99a3169756a9314637c78c5120f0d72d6d071274
Yash1234964 <ryash8500@gmail.com>
2023-01-11 16:59:06 +0530

    Collections

---
+++ CoreJava/src/Collections/Collections1.java
@@ -0,0 +1,18 @@
+package Collections;
+import java.util.*;
+
+public class Collections1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList arr=new ArrayList();
+      arr.add(10);
+      arr.add(20);
+      arr.add(30);
+      arr.add(40);
+      System.out.println(arr);
+      arr.add(2,10);
+      System.out.println(arr);
+	}
+
+}
