809fd7008bd7cd959d874dd4a22f1057aeb64c5e
Yash1234964 <ryash8500@gmail.com>
2023-01-11 17:10:08 +0530

    collections

---
+++ CoreJava/src/Collections/CollectionsDequeue.java
@@ -0,0 +1,22 @@
+package Collections;
+
+import java.util.*;
+public class CollectionsDequeue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Deque arr=new ArrayDeque();
+	      arr.add(10);
+	      arr.add(20);
+	      arr.add(30);
+	      arr.add(40);
+	      System.out.println(arr);
+	     
+	      System.out.println(arr);
+	      System.out.println(arr.removeFirst());
+	      System.out.println(arr.getFirst());
+	      
+
+	}
+
+}
